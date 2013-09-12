fa8e73ae1c713998778b8d78028d05b2cc377cd1
jwanglof <jwanglof@gmail.com>
2013-09-12 16:20:05 +0200

    Added a switch

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 1);
 // Auto load the classes that are called
 function __autoload($class_name) {
 	//class directories
@@ -29,7 +31,7 @@ function __autoload($class_name) {
 // $new_id = $a->insert($a, 'test', '(test, test2)', '(3322, 4432)');
 // $a->update($a, 'test', 'test2=1', 'id='.$new_id);
 // $a->close();
-// <?php include('StudentTrade/Views/switch.php'); 
+// 
 ?>
 
 <html>
@@ -40,6 +42,7 @@ function __autoload($class_name) {
 		<link rel="stylesheet" type="text/css" href="StudentTrade/CSS/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="StudentTrade/CSS/style.css" />
 		<link rel="shortcut icon" href="favicon.ico" />
+		<?php if (!$_GET) { ?>
 		<style type="text/css">
 			.stage {
 				position: absolute;
@@ -57,80 +60,11 @@ function __autoload($class_name) {
 				background: transparent url('StudentTrade/Img/near-clouds.png') 305px 172px repeat-x;
 			}
 		</style>
+		<?php } ?>
 	</head>
 	<body>
 		<div class='col-md-12 index top'>
-			<div>
-				<div id="far-clouds" class="stage"></div>
-				<div id="near-clouds" class="stage"></div>
-			</div>
-			<div class='row'>
-				<div class='col-md-6'>
-					<a href="index.php" title="StudentTrade.se"><img src='StudentTrade/Img/studenttrade_logo.png' class='studenttrade_logo' /></a>
-				</div>
-				<div class='col-md-6 map'>
-					<img src="StudentTrade/Img/map.png" />
-
-					<span class="linkoping"><a href="#">Linköping</a></span>
-					<span class="sthlm"><a href="#">Stockholm</a></span>
-					<span class="gbg"><a href="#">Göteborg</a></span>
-					<span class="lund"><a href="#">Lund</a></span>
-				</div>
-			</div>
-			<div id='infoText'>
-				<span class='info1'>Har du en lägenhet att hyra ut?</span>
-				<span class='info2'>En oanvänd biljett liggandes hemma?</span>
-				<span class='info3'>Eller kurslitteratur som du vill bli av med?</span>
-				<span class="what">StudentTrade.se erbjuder dig som student ett enkelt och smidigt sätt att köpa och sälja saker som kommer med studentlivet. Hos oss når du lätt andra studenter på ditt universitet.</span>
-			</div>
-			<img src="StudentTrade/Img/front_half_circle.png" class='front_half_circle' />	
-		</div>
-
-		<div class="col-md-12 index content">
-			<div class="row">
-				<div class="col-md-4 box box_image">
-					<h3>Helt gratis</h3>
-					<p>Det kostar inget för dig</p>
-					<!-- <div class="grower-wrapper">
-						<div class="grower">
-							<div class="smaller">
-								<i><i></i></i>
-							</div>
-							<div class="larger">
-								<img src="Old/img/ruta1.png" alt="Manage Expenses">
-							</div>
-						</div>
-					</div> -->
-				</div>
-				<div class="col-md-4 middle box_image">
-					<h3>Snabbt och enkelt</h3>
-					<p>Det ska vara lättåtkomligt och enkelt för dig</p>
-					<!-- <div class="grower-wrapper">
-						<div class="grower">
-							<div class="smaller">
-								<i><i></i></i>
-							</div>
-							<div class="larger">
-								<img src="Old/img/ruta2.png" alt="Grow Savings">
-							</div>
-						</div>
-					</div> -->
-				</div>
-				<div class="col-md-4 box box_image">
-					<h3>Hitta det du söker</h3>
-					<p>Det finns ett stort utbud</p>
-					<!-- <div class="grower-wrapper">
-						<div class="grower">
-							<div class="smaller">
-								<i><i></i></i>
-							</div>
-							<div class="larger">
-								<img src="Old/img/ruta3.png" alt="Receive Guidance">
-							</div>
-						</div>
-					</div> -->
-				</div>
-			</div>
+			<?php include('StudentTrade/Views/switch.php'); ?>
 		</div>
 
 		<div class="col-md-12 index footer">
@@ -160,11 +94,13 @@ function __autoload($class_name) {
 		<script src="StudentTrade/Scripts/jquery.omniwindow.js" type="text/javascript"></script>
 		<script src="StudentTrade/Scripts/scripts.js" type="text/javascript"></script>
 
+		<?php if (!$_GET) { ?>
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$('#far-clouds').pan({fps: 30, speed: 0.7, dir: 'left', depth: 30});
 				$('#near-clouds').pan({fps: 30, speed: 1, dir: 'right', depth: 70});
-			});    
+			});
 		</script>
+		<?php } ?>
 	</body>
 </html>
