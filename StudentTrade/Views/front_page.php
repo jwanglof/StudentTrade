7590d11463d11a11a8320841955e9b0970765453
jwanglof <jwanglof@gmail.com>
2013-09-12 16:19:37 +0200

    Initial commit

---
+++ StudentTrade/Views/front_page.php
@@ -0,0 +1,72 @@
+		
+			<div>
+				<div id="far-clouds" class="stage"></div>
+				<div id="near-clouds" class="stage"></div>
+			</div>
+			<div class='row'>
+				<div class='col-md-6'>
+					<a href="index.php" title="StudentTrade.se"><img src='StudentTrade/Img/studenttrade_logo.png' class='studenttrade_logo' /></a>
+				</div>
+				<div class='col-md-6 map'>
+					<img src="StudentTrade/Img/map.png" />
+
+					<span class="linkoping"><a href="#">Linköping</a></span>
+					<span class="sthlm"><a href="#">Stockholm</a></span>
+					<span class="gbg"><a href="#">Göteborg</a></span>
+					<span class="lund"><a href="#">Lund</a></span>
+				</div>
+			</div>
+			<div id='infoText'>
+				<span class='info1'>Har du en lägenhet att hyra ut?</span>
+				<span class='info2'>En oanvänd biljett liggandes hemma?</span>
+				<span class='info3'>Eller kurslitteratur som du vill bli av med?</span>
+				<span class="what">StudentTrade.se erbjuder dig som student ett enkelt och smidigt sätt att köpa och sälja saker som kommer med studentlivet. Hos oss når du lätt andra studenter på ditt universitet.</span>
+			</div>
+			<img src="StudentTrade/Img/front_half_circle.png" class='front_half_circle' />	
+		</div>
+
+		<div class="col-md-12 index content">
+			<div class="row">
+				<div class="col-md-4 box box_image">
+					<h3>Helt gratis</h3>
+					<p>Det kostar inget för dig</p>
+					<!-- <div class="grower-wrapper">
+						<div class="grower">
+							<div class="smaller">
+								<i><i></i></i>
+							</div>
+							<div class="larger">
+								<img src="Old/img/ruta1.png" alt="Manage Expenses">
+							</div>
+						</div>
+					</div> -->
+				</div>
+				<div class="col-md-4 middle box_image">
+					<h3>Snabbt och enkelt</h3>
+					<p>Det ska vara lättåtkomligt och enkelt för dig</p>
+					<!-- <div class="grower-wrapper">
+						<div class="grower">
+							<div class="smaller">
+								<i><i></i></i>
+							</div>
+							<div class="larger">
+								<img src="Old/img/ruta2.png" alt="Grow Savings">
+							</div>
+						</div>
+					</div> -->
+				</div>
+				<div class="col-md-4 box box_image">
+					<h3>Hitta det du söker</h3>
+					<p>Det finns ett stort utbud</p>
+					<!-- <div class="grower-wrapper">
+						<div class="grower">
+							<div class="smaller">
+								<i><i></i></i>
+							</div>
+							<div class="larger">
+								<img src="Old/img/ruta3.png" alt="Receive Guidance">
+							</div>
+						</div>
+					</div> -->
+				</div>
+			</div>
--- StudentTrade/Views/switch.php
+++ StudentTrade/Views/switch.php
@@ -1,12 +1,14 @@
 <?php
 	if (!$_GET) {
-		include 'index_boxes.php';
-	} else {
+		include 'front_page.php';
+	} elseif ($_GET['page']) {
 		$root_dir = getcwd() .'/StudentTrade/Views/';
 		if (is_file($root_dir . $_GET['page'] .'.php')) {
 			include $_GET['page'] .'.php';
 		} else {
 			include 'error.php';
 		}
+	} else {
+		include 'error.php';
 	}
 ?>
