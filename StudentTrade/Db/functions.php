344eaf0cf5111b7e4590d61e6ed02634e6444dda
jwanglof <jwanglof@gmail.com>
2013-09-14 19:06:35 +0200

    Added replaceSpecialChars()

--- StudentTrade/Db/functions.php
+++ StudentTrade/Db/functions.php
@@ -1,7 +1,13 @@
 <?php
-	function removeSwedishLetters($string) {
+	function replaceSwedishLetters($string) {
 		$search = array("å","ä","ö");
 		$replace = array("a","a","o");
+		return str_replace($search, $replace, $string);
+	}
+
+	function replaceSpecialChars($string) {
+		$search = array(" ");
+		$replace = array("_");
 		return str_replace($search, $replace, $string);
 	}
 ?>
