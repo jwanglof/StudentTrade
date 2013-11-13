1a2ad9d91ab0bb69ac3e398ecaa4c229c0a2325a
jwanglof <jwanglof@gmail.com>
2013-11-13 10:47:44 +0100

    Removed a lot of commented code. Move adDelete to forms.js

--- StudentTrade/Scripts/scripts.js
+++ StudentTrade/Scripts/scripts.js
@@ -39,12 +39,6 @@ $(document).ready(function() {
 	);
 
 	$("#adInfo").ready(function() {
-		// $.get("#adExtraInfo", function() {
-		// $("#adInput").hide();
-		// $("#adInput").each(function() {
-		// 	$(this).hide();
-		// });
-
 		$("#adType").click(function() {
 			var extraInfo = [1,2];
 			if ($.inArray(parseInt($("#adType").val()), extraInfo) !== -1) {
@@ -53,36 +47,6 @@ $(document).ready(function() {
 				$("#adExtraInfo").hide();
 			}
 		});
-		// });
-
-		// $("#adCategory").click(function() {
-		// 	showSubCategories($("#adCategory").val());
-		// 	// var categoryID = $("#adCategory").val();
-		// 	// $("#adInput").empty();
-
-		// 	// // Check only the first div below #adInput
-		// 	// // $("#adInput > div").each(function() {
-		// 	// // $("#adInput").children().each(function() {
-		// 	// $("#adInputHidden").find("."+ categoryID).each(function() {
-		// 	// 	// alert($(this));
-		// 	// 	// $("#adInput").append($(this));
-		// 	// 	fucku($(this));
-		// 	// 	$("#adInput").height($("#adInput").height + 1);
-		// 	// 	$("#adInput").height($("#adInput").height - 1);
-		// 	// });
-		// 	// // console.log($("#adInput").find("."+ categoryID));
-		// 	// // $("#adInput").find(".form-group").hasClass(categoryID).each(function() {
-		// 	// 	// alert($(this));
-		// 	// 	// $(this).show();
-		// 	// 	// if ($(this).hasClass(categoryID)) {
-		// 	// 	// 	$(this).show();
-		// 	// 	// } else {
-		// 	// 	// 	$(this).hide();
-		// 	// 	// }
-		// 	// // });
-
-		// 	// // $("#adInput").show();
-		// });
 	});
 
 	$("#requestCity").click(function() {
@@ -124,25 +88,6 @@ $(document).ready(function() {
 		}
 	});
 });
-
-
-
-// $(document).on("click", "#adCategory", function(e) {
-// 	showSubCategories($("#adCategory").val());
-// });
-
-// function showSubCategories(categoryID) {
-// 	// $("#adInput").empty();
-// 	console.log($("#adInput").find("."+ categoryID));
-// 	$("#adInput").find("."+ categoryID).each(function() {
-// 		$(this).show();
-// 	});
-// 	// $("#adInput").show();
-// }
-
-function closeModal(modalID) {
-	$(modalID).modal("hide");
-}
 
 function sendFormWithAjax(form) {
 	var postData = $(form).serializeArray();
@@ -311,33 +256,5 @@ $(document).on("click", "#adReport", function(e) {
 		backdrop: true,
 		closeButton: true,
 		animate: true
-	});
-});
-
-$(document).on("click", "#adDelete", function(e) {
-	bootbox.prompt("Ange den borttagninskod som du fått via e-post", function(result) {
-		if (result != null) {
-			request = $.ajax({
-				type: "post",
-				url: getAjaxURL("update"),
-				data: {update: "adActive", aid: gup("aid"), removeCode: result}
-			});
-			console.log(result);
-
-			request.done(function(response, textStatus, jqXHR) {
-				console.log(textStatus);
-				console.log(response);
-				if (response == 1) {
-					bootbox.alert("Annonsen borttagen");
-					window.location.replace("front.php?page=latest");
-				}
-				else
-					bootbox.alert("Fel kod angiven");
-			});
-			request.fail(function(jqXHR, textStatus, errorThrown) {
-				console.log(jqXHR);
-				bootbox.alert(errorThrown);
-			});
-		}
 	});
 });
