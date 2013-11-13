22133096e0661482fefc1085d14b55a12fcbe9fe
jwanglof <jwanglof@gmail.com>
2013-11-13 11:48:26 +0100

    Good old trash

--- StudentTrade/Scripts/Trash/trash_from_scripts.js
+++ StudentTrade/Scripts/Trash/trash_from_scripts.js
@@ -27,3 +27,93 @@
 	// 		showAdCategoryInputs($(this).val());
 	// 	}
 	// });
+
+	// $(document).on("submit", "#adDeleteForm", function(e) {
+// 	alert(2);
+// 	// request = $.ajax({
+// 	// 	type: "post",
+// 	// 	url: getAjaxURL("update"),
+// 	// 	data: {update: "adActive", aid: gup("aid"), removeCode: result}
+// 	// });
+// 	// // console.log(result);
+
+// 	// request.done(function(response, textStatus, jqXHR) {
+// 	// 	// console.log(textStatus);
+// 	// 	// console.log(response);
+// 	// 	if (response == 1) {
+// 	// 		bootbox.alert("Annonsen borttagen");
+// 	// 		window.location.replace("front.php?page=latest");
+// 	// 	}
+// 	// 	else
+// 	// 		bootbox.alert("Fel kod angiven");
+// 	// });
+// 	// request.fail(function(jqXHR, textStatus, errorThrown) {
+// 	// 	// console.log(jqXHR);
+// 	// 	bootbox.alert(errorThrown);
+// 	// });
+// });
+
+	// $(document).on("click", "#adCategory", function(e) {
+// 	showSubCategories($("#adCategory").val());
+// });
+
+// function showSubCategories(categoryID) {
+// 	// $("#adInput").empty();
+// 	console.log($("#adInput").find("."+ categoryID));
+// 	$("#adInput").find("."+ categoryID).each(function() {
+// 		$(this).show();
+// 	});
+// 	// $("#adInput").show();
+// }
+
+// function closeModal(modalID) {
+// 	$(modalID).modal("hide");
+// }
+
+
+// $("#adInfo").ready(function() {
+// 		// $.get("#adExtraInfo", function() {
+// 		// $("#adInput").hide();
+// 		// $("#adInput").each(function() {
+// 		// 	$(this).hide();
+// 		// });
+
+// 		$("#adType").click(function() {
+// 			var extraInfo = [1,2];
+// 			if ($.inArray(parseInt($("#adType").val()), extraInfo) !== -1) {
+// 				$("#adExtraInfo").show();
+// 			} else {
+// 				$("#adExtraInfo").hide();
+// 			}
+// 		});
+// 		// });
+
+// 		// $("#adCategory").click(function() {
+// 		// 	showSubCategories($("#adCategory").val());
+// 		// 	// var categoryID = $("#adCategory").val();
+// 		// 	// $("#adInput").empty();
+
+// 		// 	// // Check only the first div below #adInput
+// 		// 	// // $("#adInput > div").each(function() {
+// 		// 	// // $("#adInput").children().each(function() {
+// 		// 	// $("#adInputHidden").find("."+ categoryID).each(function() {
+// 		// 	// 	// alert($(this));
+// 		// 	// 	// $("#adInput").append($(this));
+// 		// 	// 	fucku($(this));
+// 		// 	// 	$("#adInput").height($("#adInput").height + 1);
+// 		// 	// 	$("#adInput").height($("#adInput").height - 1);
+// 		// 	// });
+// 		// 	// // console.log($("#adInput").find("."+ categoryID));
+// 		// 	// // $("#adInput").find(".form-group").hasClass(categoryID).each(function() {
+// 		// 	// 	// alert($(this));
+// 		// 	// 	// $(this).show();
+// 		// 	// 	// if ($(this).hasClass(categoryID)) {
+// 		// 	// 	// 	$(this).show();
+// 		// 	// 	// } else {
+// 		// 	// 	// 	$(this).hide();
+// 		// 	// 	// }
+// 		// 	// // });
+
+// 		// 	// // $("#adInput").show();
+// 		// });
+// 	});
