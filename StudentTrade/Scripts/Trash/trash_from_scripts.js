43268d81b53a77f07898b4e6105f2ef8cd7d8aaa
jwanglof <jwanglof@gmail.com>
2013-11-13 09:48:12 +0100

    Trash-scripts

---
+++ StudentTrade/Scripts/Trash/trash_from_scripts.js
@@ -0,0 +1,29 @@
+	// var currentCity;
+	// $("#city").ready(function() {
+	// 	// Select the correct city if a city is chosen in $_GET
+	// 	if ($('#city').children(':selected').val() > 0) {
+	// 		currentCity = $('#city').children(':selected').val(); 
+	// 		showCampuses($('#city').children(':selected').val());
+	// 	}
+	// });
+	// $("#city").click(function() {
+	// 	if (currentCity != $(this).val()) {
+	// 		currentCity = $(this).val();
+	// 		showCampuses($(this).val());
+	// 	}
+	// });
+
+	// var currentCategory;
+	// $("#adCategory").ready(function() {
+	// 	// Select the correct category if a category is chosen in $_GET
+	// 	if ($('#adCategory').children(':selected').val() > 0) {
+	// 		currentCategory = $('#adCategory').children(':selected').val();
+	// 		showAdCategoryInputs($('#adCategory').children(':selected').val());
+	// 	}
+	// });
+	// $("#adCategory").click(function() {
+	// 	if (currentCategory != $(this).val()) {
+	// 		currentCategory = $(this).val();
+	// 		showAdCategoryInputs($(this).val());
+	// 	}
+	// });
