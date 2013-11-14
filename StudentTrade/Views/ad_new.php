0eb8954eaf4fac40bb468a8d98912bfbc6183803
Johan Wanglof <jwanglof@gmail.com>
2013-11-14 20:20:49 +0100

    Changed col-lg- to col-xs

--- StudentTrade/Views/ad_new.php
+++ StudentTrade/Views/ad_new.php
@@ -14,29 +14,29 @@ $dbh = null;
 			<legend>Personuppgifter</legend>
 
 			<div class="form-group">
-				<label for="name" class="col-lg-1 control-label">Namn *</label>
-				<div class="col-lg-5">
+				<label for="name" class="col-xs-1 control-label">Namn *</label>
+				<div class="col-xs-5">
 					<input type="text" class="form-control" id="name" name="name" placeholder="Namn">
 				</div>
 			</div>
 
 			<div class="form-group">
-				<label for="email" class="col-lg-1 control-label">E-post *</label>
-				<div class="col-lg-5">
+				<label for="email" class="col-xs-1 control-label">E-post *</label>
+				<div class="col-xs-5">
 					<input type="email" class="form-control" id="email" name="email" placeholder="E-post">
 				</div>
 			</div>
 
 			<div class="form-group">
-				<label for="phonenumber" class="col-lg-1 control-label">Telefonnummer</label>
-				<div class="col-lg-5">
+				<label for="phonenumber" class="col-xs-1 control-label">Telefonnummer</label>
+				<div class="col-xs-5">
 					<input type="text" class="form-control" id="phonenumber" name="phonenumber" placeholder="Telefonnummer">
 				</div>
 			</div>
 
 			<div class="form-group">
-				<label for="city" class="col-lg-1 control-label"><img src="StudentTrade/Img/ajax-loader.gif" class="ajaxLoader ajaxCity" /> Stad *</label>
-				<div class="col-lg-5">
+				<label for="city" class="col-xs-1 control-label"><img src="StudentTrade/Img/ajax-loader.gif" class="ajaxLoader ajaxCity" /> Stad *</label>
+				<div class="col-xs-5">
 					<select id="city" name="city" class="form-control">
 						<?php
 						foreach ($cities as $value) {
@@ -53,8 +53,8 @@ $dbh = null;
 			</div>
 
 			<div class="form-group">
-				<label for="campus" class="col-lg-1 control-label">Campus</label>
-				<div class="col-lg-5">
+				<label for="campus" class="col-xs-1 control-label">Campus</label>
+				<div class="col-xs-5">
 					<select id="campus" name="campus" class="form-control">
 					</select>
 				</div>
@@ -65,8 +65,8 @@ $dbh = null;
 
 			<div id="adInfo">
 				<div class="form-group">
-					<label for="adType" class="col-lg-1 control-label">Typ av annons *</label>
-					<div class="col-lg-5">
+					<label for="adType" class="col-xs-1 control-label">Typ av annons *</label>
+					<div class="col-xs-5">
 						<select id="adType" name="adType" class="form-control">
 							<?php
 							foreach ($adTypes as $value) {
@@ -79,8 +79,8 @@ $dbh = null;
 			</div>
 
 			<div class="form-group">
-				<label for="adCategory" class="col-lg-1 control-label"><img src="StudentTrade/Img/ajax-loader.gif" class="ajaxLoader ajaxCategory" /> Kategori *</label>
-				<div class="col-lg-5">
+				<label for="adCategory" class="col-xs-1 control-label"><img src="StudentTrade/Img/ajax-loader.gif" class="ajaxLoader ajaxCategory" /> Kategori *</label>
+				<div class="col-xs-5">
 					<select id="adCategory" name="adCategory" class="form-control">
 						<option value="0">Kategori</option>
 						<?php
@@ -102,8 +102,8 @@ $dbh = null;
 			/*foreach ($adSubCategories as $key => $value) {
 			?>
 				<div class="form-group <?php echo $value['fk_adTypeInfo_adCategory']; ?>">
-					<label for="<?php echo $value['short_name']; ?>" class="col-lg-1 control-label"><?php echo $value['name']; ?></label>
-					<div class="col-lg-5">
+					<label for="<?php echo $value['short_name']; ?>" class="col-xs-1 control-label"><?php echo $value['name']; ?></label>
+					<div class="col-xs-5">
 						<input type="text" class="form-control" id="<?php echo $value['short_name']; ?>" name="<?php echo $value['short_name']; ?>" placeholder="<?php echo $value['name']; ?>">
 					</div>
 				</div>
@@ -114,23 +114,23 @@ $dbh = null;
 
 			<div id="adExtraInfo">
 				<div class="form-group">
-					<label for="price" class="col-lg-1 control-label">Pris (SEK) *</label>
-					<div class="col-lg-5">
+					<label for="price" class="col-xs-1 control-label">Pris (SEK) *</label>
+					<div class="col-xs-5">
 						<input type="number" class="form-control" id="price" name="price" value="0" placeholder="Pris">
 					</div>
 				</div>
 			</div>
 
 			<div class="form-group">
-				<label for="title" class="col-lg-1 control-label">Rubrik *</label>
-				<div class="col-lg-5">
+				<label for="title" class="col-xs-1 control-label">Rubrik *</label>
+				<div class="col-xs-5">
 					<input type="text" class="form-control" id="title" name="title" placeholder="Rubrik">
 				</div>
 			</div>
 
 			<div class="form-group">
-				<label for="info" class="col-lg-1 control-label">Beskrivning *</label>
-				<div class="col-lg-5">
+				<label for="info" class="col-xs-1 control-label">Beskrivning *</label>
+				<div class="col-xs-5">
 					<textarea id="info" name="info" class="form-control" style="width: 100%; height: 200px;"></textarea>
 				</div>
 			</div>
