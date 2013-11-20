18cbd8ae60f0158953fe615ad9315e6e8ad41de6
jwanglof <jwanglof@gmail.com>
2013-11-20 17:07:20 +0100

    Pagination stufffffffffffffffff

--- StudentTrade/Class/Blaffs.php
+++ StudentTrade/Class/Blaffs.php
@@ -10,9 +10,16 @@ class Blaffs extends DbConfig {
 
 	private $currentPageNumber = 1;
 
-	public function __construct($totalRows, $itemsPerPage) {
+	private $previousPage;
+
+	public $links = "";
+
+	private $URL;
+
+	public function __construct($totalRows, $itemsPerPage, $URL) {
 		$this->totalRows = $totalRows; 
 		$this->itemsPerPage = $itemsPerPage;
+		$this->URL = $URL;
 
 		$this->lastPageNumber = ceil($totalRows / $itemsPerPage);
 
@@ -34,7 +41,7 @@ class Blaffs extends DbConfig {
 		return $this->currentPageNumber;
 	}
 
-	public function setLimit() {
+	public function getAds() {
 		//$stmt = $this->dbh->prepare("SELECT * FROM ad ORDER BY id LIMIT :limit OFFSET :offset");
 		$hej = ($this->currentPageNumber - 1) * $this->itemsPerPage;
 
@@ -44,6 +51,8 @@ class Blaffs extends DbConfig {
 
 		$stmt->execute();
 		$this->dbh = null;
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
 	public function getCurrentPage() {
@@ -54,10 +63,40 @@ class Blaffs extends DbConfig {
 		return $this->lastPageNumber;
 	}
 
+	public function getNextPage() {
+		// Next page
+		if ($this->currentPageNumber != $this->lastPageNumber)
+			$this->links .= "<a href=\"". $this->URL ."&pageNo=". ($this->currentPageNumber+1) ."\">Nästa</a>"; //" ". $this->currentPageNumber + 1 ." ";
+	}
+
 	public function getPages() {
 		if ($this->lastPageNumber != 1) {
-			
+			if ($this->currentPageNumber > 1) {
+				$this->previousPage = $this->currentPageNumber - 1;
+
+				$this->links .= "<a href=\"". $this->URL ."&pageNo=". $this->previousPage ."\">Förra</a> ";
+
+				for ($i = $this->currentPageNumber - 4; $i < $this->currentPageNumber; $i++) {
+					if ($i > 0)
+						$this->links .= " ". $i ." ";
+				}
+			}
+
+			$this->links .= " >". $this->currentPageNumber ."< ";
+
+			for ($i = $this->currentPageNumber + 1; $i <= $this->lastPageNumber; $i++) {
+				$this->links .= " ". $i ." ";
+
+				if ($i >= $this->currentPageNumber + 4)
+					break;
+			}
+
+			// Next page
+			if ($this->currentPageNumber != $this->lastPageNumber)
+				$this->links .= "<a href=\"". $this->URL ."&pageNo=". ($this->currentPageNumber+1) ."\">Nästa</a>"; //" ". $this->currentPageNumber + 1 ." ";
 		}
+
+		return $this->links;
 	}
 }
 ?>
